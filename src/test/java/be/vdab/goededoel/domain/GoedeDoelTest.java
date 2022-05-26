fa6fe91491e682970b7c52c1dd913fa80004ea6e
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-05-27 00:29:25 +0200

    spotbugs equals

--- src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/domain/GoedeDoelTest.java
@@ -39,4 +39,9 @@ class GoedeDoelTest {
 		assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
 	}
 
+	@Test
+	void gelijkeDoelGevenHebbenDezelfdeHashCode() {
+		assertThat(doel).hasSameHashCodeAs(new GoedeDoel(NAAM));
+	}
+
 }
